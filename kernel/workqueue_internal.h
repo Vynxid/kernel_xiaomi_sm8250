40764bded5434620bd795d50e295966b35cec573
Tejun Heo <tj@kernel.org>
2023-05-17 17:02:08 -1000

    workqueue: Re-order struct worker fields
    struct worker was laid out with the intent that all fields that are modified
    for each work item execution are in the first cacheline. However, this
    hasn't been true for a while with the addition of ->last_func. Let's just
    collect hot fields together at the top.
    
    Move ->sleeping in the hole after ->current_color and move ->lst_func right
    below. While at it, drop the cacheline comment which isn't useful anymore.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Lai Jiangshan <jiangshanlai@gmail.com>

--- kernel/workqueue_internal.h
+++ kernel/workqueue_internal.h
@@ -31,9 +31,12 @@ struct worker {
 	struct work_struct	*current_work;	/* L: work being processed */
 	work_func_t		current_func;	/* L: current_work's fn */
 	struct pool_workqueue	*current_pwq; /* L: current_work's pwq */
-	struct list_head	scheduled;	/* L: scheduled works */
+	int			sleeping;	/* None */
 
-	/* 64 bytes boundary on 64bit, 32 on 32bit */
+	/* used by the scheduler to determine a worker's last known identity */
+	work_func_t		last_func;
+
+	struct list_head	scheduled;	/* L: scheduled works */
 
 	struct task_struct	*task;		/* I: worker task */
 	struct worker_pool	*pool;		/* A: the associated pool */
@@ -44,7 +47,6 @@ struct worker {
 	unsigned long		last_active;	/* L: last active timestamp */
 	unsigned int		flags;		/* X: flags */
 	int			id;		/* I: worker id */
-	int			sleeping;	/* None */
 
 	/*
 	 * Opaque string set with work_set_desc().  Printed out with task
@@ -54,9 +56,6 @@ struct worker {
 
 	/* used only by rescuers to point to the target workqueue */
 	struct workqueue_struct	*rescue_wq;	/* I: the workqueue to rescue */
-
-	/* used by the scheduler to determine a worker's last known identity */
-	work_func_t		last_func;
 };
 
 /**
