e94263790b782e62dfab19c8314351255774bdb0
Tejun Heo <tj@kernel.org>
2023-05-17 17:02:08 -1000

    workqueue: Automatically mark CPU-hogging work items CPU_INTENSIVE
    If a per-cpu work item hogs the CPU, it can prevent other work items from
    starting through concurrency management. A per-cpu workqueue which intends
    to host such CPU-hogging work items can choose to not participate in
    concurrency management by setting %WQ_CPU_INTENSIVE; however, this can be
    error-prone and difficult to debug when missed.
    
    This patch adds an automatic CPU usage based detection. If a
    concurrency-managed work item consumes more CPU time than the threshold
    (10ms by default) continuously without intervening sleeps, wq_worker_tick()
    which is called from scheduler_tick() will detect the condition and
    automatically mark it CPU_INTENSIVE.
    
    The mechanism isn't foolproof:
    
    * Detection depends on tick hitting the work item. Getting preempted at the
      right timings may allow a violating work item to evade detection at least
      temporarily.
    
    * nohz_full CPUs may not be running ticks and thus can fail detection.
    
    * Even when detection is working, the 10ms detection delays can add up if
      many CPU-hogging work items are queued at the same time.
    
    However, in vast majority of cases, this should be able to detect violations
    reliably and provide reasonable protection with a small increase in code
    complexity.
    
    If some work items trigger this condition repeatedly, the bigger problem
    likely is the CPU being saturated with such per-cpu work items and the
    solution would be making them UNBOUND. The next patch will add a debug
    mechanism to help spot such cases.
    
    v4: Documentation for workqueue.cpu_intensive_thresh_us added to
        kernel-parameters.txt.
    
    v3: Switch to use wq_worker_tick() instead of hooking into preemptions as
        suggested by Peter.
    
    v2: Lai pointed out that wq_worker_stopping() also needs to be called from
        preemption and rtlock paths and an earlier patch was updated
        accordingly. This patch adds a comment describing the risk of infinte
        recursions and how they're avoided.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Lai Jiangshan <jiangshanlai@gmail.com>

--- kernel/workqueue_internal.h
+++ kernel/workqueue_internal.h
@@ -31,6 +31,7 @@ struct worker {
 	struct work_struct	*current_work;	/* K: work being processed and its */
 	work_func_t		current_func;	/* K: function */
 	struct pool_workqueue	*current_pwq; /* K: pwq */
+	u64			current_at;	/* K: runtime at start or last wakeup */
 
 	int			sleeping;	/* S: is worker sleeping? */
 
@@ -75,6 +76,7 @@ static inline struct worker *current_wq_worker(void)
  */
 void wq_worker_running(struct task_struct *task);
 void wq_worker_sleeping(struct task_struct *task);
+void wq_worker_tick(struct task_struct *task);
 work_func_t wq_worker_last_func(struct task_struct *task);
 
 #endif /* _KERNEL_WORKQUEUE_INTERNAL_H */
